84be345e3f81d64806c933c016cdfa7e2d2d4bad
Eric Schwartz <eric.schwartz@theironyard.com>
2017-09-15 14:07:08 -0700

    stuffs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title: string = 'app';
+
+  selectedThing: object;
+
+  favoriteThings = [
+    {id: 1, name: "ice cream"},
+    {id: 2, name: "more ice cream"},
+    {id: 3, name: "even more ice cream"},
+    {id: 4, name: "infinity ice cream"},
+    {id: 5, name: "children crying"},
+    {id: 6, name: "living with Curtis"},
+  ]
+
+  clickedThing(thingIClicked){
+    this.selectedThing = thingIClicked;
+  }
+
 }
